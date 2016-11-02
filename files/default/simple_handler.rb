2c83d6062fae8c4e909efc6642838db4b749b711
Steve Smith <stever.smith@expeditors.com>
2016-11-02 08:08:36 -0700

    added updated resources

--- files/default/simple_handler.rb
+++ files/default/simple_handler.rb
@@ -1,4 +1,5 @@
 require 'chef/handler'
+#require 'json'
 
 class Chef
   class Handler
@@ -6,6 +7,10 @@ class Chef
     class SimpleHandler < Chef::Handler
       def report
         Chef::Log.warn("SimpleHandler: @run_status.success? => #{@run_status.success?}")
+
+        Chef::Log.warn("Updated Resources:")
+        @run_status.updated_resources.each {|r| Chef::Log.warn("  #{r.to_s}")}
+
       end
     end
   end
